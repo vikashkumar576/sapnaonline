a81969e6819dad01838b5487e686d6d124a352b2
vikash <vikashkumar576git.com>
2023-12-03 12:03:42 +0530

    product page with slug

--- components/products/slug/index.jsx
+++ components/products/slug/index.jsx
@@ -1,13 +1,19 @@
+import { useEffect, useState } from "react";
 import Layout from "@/components/shared/layout"
 import { useRouter } from "next/router"
+import Image from "next/image"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/modules';
 
 const Slug = ()=>{
     const router = useRouter();
 
     return(
-        <Layout title={`Buy ${router.query.slug.toLowerCase().split("-").join(" ")}`}>
+        <Layout title={`Buy ${router.query.slug && router.query.slug.toLowerCase().split("-").join(" ")}`}>
             <div className="grid md:grid-cols-10 gap-12 w-[88%] mx-auto">
-                <div className="col-span-4 p-10 bg-black">
+                <div className="col-span-4 p-10 bg-white">
                     
                 </div>
                 <div className="col-span-6 p-10 bg-black"></div>
