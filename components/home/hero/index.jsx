e8995b0a58d97bfc948cc5fe40b6cea18ccf12d0
vikash <vikashkumar576git.com>
2023-12-03 21:29:15 +0530

    home modified

--- components/home/hero/index.jsx
+++ components/home/hero/index.jsx
@@ -3,7 +3,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation, Pagination, Mousewheel, Keyboard, Autoplay } from 'swiper/modules';
-import Test from "../test";
 
 const offerBanners = [
     {
